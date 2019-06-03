d503c7a4c50011a535f0209cf2f634de3836d059
Seoyeon Kim <catneru@naver.com>
2019-06-03 09:55:12 +0900

    #2 ListView 항목 추가, 선택 코드 구현

--- ListBox/Form1.Designer.cs
+++ ListBox/Form1.Designer.cs
@@ -28,90 +28,93 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.label3 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            this.listView1 = new System.Windows.Forms.ListView();
+            this.lblName = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtAge = new System.Windows.Forms.TextBox();
+            this.lblAge = new System.Windows.Forms.Label();
+            this.txtJob = new System.Windows.Forms.TextBox();
+            this.lblJob = new System.Windows.Forms.Label();
+            this.BtnAdd = new System.Windows.Forms.Button();
+            this.lvView = new System.Windows.Forms.ListView();
             this.chName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.chAge = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.chJob = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.SuspendLayout();
             // 
-            // label1
+            // lblName
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 228);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(37, 12);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "이름 :";
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(12, 228);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(37, 12);
+            this.lblName.TabIndex = 1;
+            this.lblName.Text = "이름 :";
             // 
-            // textBox1
+            // txtName
             // 
-            this.textBox1.Location = new System.Drawing.Point(56, 225);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(188, 21);
-            this.textBox1.TabIndex = 2;
+            this.txtName.Location = new System.Drawing.Point(56, 225);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(188, 21);
+            this.txtName.TabIndex = 2;
+            this.txtName.TextChanged += new System.EventHandler(this.TextBox1_TextChanged);
             // 
-            // textBox2
+            // txtAge
             // 
-            this.textBox2.Location = new System.Drawing.Point(56, 252);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(188, 21);
-            this.textBox2.TabIndex = 4;
+            this.txtAge.Location = new System.Drawing.Point(56, 252);
+            this.txtAge.Name = "txtAge";
+            this.txtAge.Size = new System.Drawing.Size(188, 21);
+            this.txtAge.TabIndex = 4;
             // 
-            // label2
+            // lblAge
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 255);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(37, 12);
-            this.label2.TabIndex = 3;
-            this.label2.Text = "나이 :";
+            this.lblAge.AutoSize = true;
+            this.lblAge.Location = new System.Drawing.Point(12, 255);
+            this.lblAge.Name = "lblAge";
+            this.lblAge.Size = new System.Drawing.Size(37, 12);
+            this.lblAge.TabIndex = 3;
+            this.lblAge.Text = "나이 :";
             // 
-            // textBox3
+            // txtJob
             // 
-            this.textBox3.Location = new System.Drawing.Point(56, 279);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(188, 21);
-            this.textBox3.TabIndex = 6;
+            this.txtJob.Location = new System.Drawing.Point(56, 279);
+            this.txtJob.Name = "txtJob";
+            this.txtJob.Size = new System.Drawing.Size(188, 21);
+            this.txtJob.TabIndex = 6;
             // 
-            // label3
+            // lblJob
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(12, 282);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(37, 12);
-            this.label3.TabIndex = 5;
-            this.label3.Text = "직업 :";
+            this.lblJob.AutoSize = true;
+            this.lblJob.Location = new System.Drawing.Point(12, 282);
+            this.lblJob.Name = "lblJob";
+            this.lblJob.Size = new System.Drawing.Size(37, 12);
+            this.lblJob.TabIndex = 5;
+            this.lblJob.Text = "직업 :";
             // 
-            // button1
+            // BtnAdd
             // 
-            this.button1.Location = new System.Drawing.Point(253, 225);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(97, 75);
-            this.button1.TabIndex = 7;
-            this.button1.Text = "추가";
-            this.button1.UseVisualStyleBackColor = true;
+            this.BtnAdd.Location = new System.Drawing.Point(253, 225);
+            this.BtnAdd.Name = "BtnAdd";
+            this.BtnAdd.Size = new System.Drawing.Size(97, 75);
+            this.BtnAdd.TabIndex = 7;
+            this.BtnAdd.Text = "추가";
+            this.BtnAdd.UseVisualStyleBackColor = true;
+            this.BtnAdd.Click += new System.EventHandler(this.BtnAdd_Click);
             // 
-            // listView1
+            // lvView
             // 
-            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.lvView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
             this.chName,
             this.chAge,
             this.chJob});
-            this.listView1.FullRowSelect = true;
-            this.listView1.GridLines = true;
-            this.listView1.Location = new System.Drawing.Point(13, 13);
-            this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(337, 206);
-            this.listView1.TabIndex = 8;
-            this.listView1.UseCompatibleStateImageBehavior = false;
-            this.listView1.View = System.Windows.Forms.View.Details;
+            this.lvView.FullRowSelect = true;
+            this.lvView.GridLines = true;
+            this.lvView.Location = new System.Drawing.Point(13, 13);
+            this.lvView.Name = "lvView";
+            this.lvView.Size = new System.Drawing.Size(337, 206);
+            this.lvView.TabIndex = 8;
+            this.lvView.UseCompatibleStateImageBehavior = false;
+            this.lvView.View = System.Windows.Forms.View.Details;
+            this.lvView.SelectedIndexChanged += new System.EventHandler(this.LvView_SelectedIndexChanged);
             // 
             // chName
             // 
@@ -133,18 +136,19 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(362, 304);
-            this.Controls.Add(this.listView1);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lvView);
+            this.Controls.Add(this.BtnAdd);
+            this.Controls.Add(this.txtJob);
+            this.Controls.Add(this.lblJob);
+            this.Controls.Add(this.txtAge);
+            this.Controls.Add(this.lblAge);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.lblName);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.Text = "리스트 추가";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -152,14 +156,14 @@
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtAge;
+        private System.Windows.Forms.Label lblAge;
+        private System.Windows.Forms.TextBox txtJob;
+        private System.Windows.Forms.Label lblJob;
+        private System.Windows.Forms.Button BtnAdd;
+        private System.Windows.Forms.ListView lvView;
         private System.Windows.Forms.ColumnHeader chName;
         private System.Windows.Forms.ColumnHeader chAge;
         private System.Windows.Forms.ColumnHeader chJob;
--- ListBox/Form1.cs
+++ ListBox/Form1.cs
@@ -11,9 +11,76 @@ namespace ListBox
 {
     public partial class Form1 : Form
     {
+        string strName;
+        string strAge;
+        string strJob;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private bool TextCheck()
+        {
+            if (this.txtName.Text == "")
+            {
+                MessageBox.Show("이름을 입력해주세요!", "알림", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                this.txtName.Focus();
+                return false;
+            }
+            if (this.txtJob.Text == "")
+            {
+                MessageBox.Show("직업을 입력해주세요!", "알림", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                this.txtJob.Focus();
+                return false;
+            }
+            if (this.txtAge.Text == "")
+            {
+                MessageBox.Show("나이를 입력해주세요!", "알림", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                this.txtAge.Focus();
+                return false;
+            }
+            return true;
+        }
+
+        private void BtnAdd_Click(object sender, EventArgs e)
+        {
+            if (TextCheck())
+            {
+                this.strName = this.txtName.Text;
+                this.strAge = this.txtAge.Text;
+                this.strJob = this.txtJob.Text;
+                ListViewItem lvi =          //리스트뷰에 들어갈 항목 생성
+                     new ListViewItem(
+                         new string[] { strName, strAge, strJob }
+                     );
+
+
+                this.lvView.Items.Add(lvi);
+                this.txtName.Text = "";
+                this.txtAge.Text = "";
+                this.txtJob.Text = "";
+                this.txtName.Focus();
+            }
+        }
+
+        private void LvView_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ListViewItem lvi = this.lvView.SelectedItems[0];
+            string name = lvi.SubItems[0].Text;
+            String age = lvi.SubItems[1].Text;
+            String job = lvi.SubItems[2].Text;
+
+            MessageBox.Show("이름 : " + name + "\n나이 : " + age + "\n직업 : " + job, "자세히 보기", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
         }
     }
 }
